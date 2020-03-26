50feb253df1218e38f5d4bc5db73f4d725d7113a
itzhalpepe <leon.buehrle@t-online.de>
2020-03-26 16:34:16 +0100

    if Abfrage

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,18 +1,18 @@
-//alert("Hello world");
+// alert("Hello world");
 
-//let message = "Hello world";
-//alert(message);
-//message = "Hallo Welt";
-//alert(message);
+// let message = "Hello world";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
 
-const Link_Color = "#ff000";
-console.log("Link bitte in der Farbe", Link_Color);
+const LINK_COLOR = "#ff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
 
 let highscore = 520233;
 console.log(highscore / 10);
 
 let firstname = "John";
-let lastname = "Smith";
+let lastname = 'Smith';
 console.log("Name: ", firstname, lastname);
 
 let fullname = 'Jeffrey "The Dude" Lebowski';
@@ -25,20 +25,20 @@ let isOver18 = true;
 console.log(isOver18);
 
 let age = 19;
-console.log("über 18", age > 18);
+console.log("über 18?", age > 18);
 
-let participants = ["John", "Jane", "Max"];
+let participants = [ "John", "Jane", "Max" ];
 console.log(participants);
-console.log("Einträge im Array:", participants.length);
-console.log(participitants[1]);
+console.log("Einträge im Array: ", participants.length);
+console.log(participants[2]);
 
-let gameHighscores = [2099, 3010, 3333, 5000];
+let gameHighscores = [2099, 3010, 3333, 5000 ];
 console.log(gameHighscores);
 
 let user = {
-    firstname: "John";
-    lastname: "Smith";
-    age: 25;
+    firstname: "John",
+    lastname: "Smith",
+    age: 25
 };
 
 console.log(user);
@@ -49,7 +49,17 @@ console.log(user);
 
 let a = 2;
 let b = 4;
-console.log(a+b)
+console.log(a+b);
 console.log(b/(a-1));
 a++;
 console.log(a);
+
+// let myAge = prompt("Wie alt bist du?");
+// console.log(`Du bist ${myAge} Jahre alt.`);
+// console.log(`über 18? ${myAge > 18}`);
+
+// if (myAge > 18) {
+//     console.log("Glückwunsch über 18");
+// } else {
+//     console.log("Leider unter 18");
+// }
