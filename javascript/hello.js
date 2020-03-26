a24239552a65b46fad668e8dadedb75fdbeb7fd1
itzhalpepe <leon.buehrle@t-online.de>
2020-03-26 17:10:51 +0100

    foreach mit Funktionsaufruf

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,4 +1,3 @@
-  
 // alert("Hello world");
 
 // let message = "Hello world";
@@ -88,4 +87,27 @@ for (let j = 0; j < participants.length; j++) {
 
 participants.forEach(participant => {
     console.log(`Teilnehmer*in ${participant}`);
+});
+
+// Funktionen
+
+function showAge(birthYear) {
+    console.log(`Du bist ca. ${2020 - birthYear} Jahre alt.`);
+}
+
+showAge(1964);
+showAge(1977);
+
+function calcAge(birthYear) {
+    return 2020 - birthYear;
+}
+
+console.log(`Max ist ${calcAge(1977)} Jahre alt (ca.)`);
+console.log(`John ist ${calcAge(1988)} Jahre alt (ca.)`);
+
+let birthYears = [ 1964, 1977, 1980, 2001, 2004 ];
+console.log(birthYears);
+
+birthYears.forEach(year => {
+    console.log(`Geboren ${year}, heute ca. ${calcAge(year)} Jahre alt.`);
 });
