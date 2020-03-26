9627330fb11188a201a7d3ed7d2605bc742eb9b9
itzhalpepe <leon.buehrle@t-online.de>
2020-03-26 16:05:29 +0100

    Highscore

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,22 @@
 //alert("Hello world");
+
+//let message = "Hello world";
+//alert(message);
+//message = "Hallo Welt";
+//alert(message);
+
+const Link_Color = "#ff000";
+console.log("Link bitte in der Farbe", Link_Color);
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = "Smith";
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
+let template = ´Dein Highscore sind ${highscore} Punkte´;
+console.log(template)
