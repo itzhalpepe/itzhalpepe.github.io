7628bc738b0f9af60013afe30fd462043bd59a2b
itzhalpepe <leon.buehrle@t-online.de>
2020-03-28 18:29:50 +0100

    beautify

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,12 @@
 //{var mymap = L.map('map').setView([-41.327222, 174.805278], 13);
 
 //L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  //  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //maxZoom: 18,
-    //id: 'mapbox/streets-v11',
-    //tileSize: 512,
-    //zoomOffset: -1,
-    //accessToken: 'pk.eyJ1IjoiY2d1dGgiLCJhIjoiY2syZGVpbzFkMDJpeDNibGtuNnhucDJnMiJ9.YbgYZkJ7Wcb_6wp6GvupHA'
+//  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//maxZoom: 18,
+//id: 'mapbox/streets-v11',
+//tileSize: 512,
+//zoomOffset: -1,
+//accessToken: 'pk.eyJ1IjoiY2d1dGgiLCJhIjoiY2syZGVpbzFkMDJpeDNibGtuNnhucDJnMiJ9.YbgYZkJ7Wcb_6wp6GvupHA'
 //}).addTo(mymap);
 
 //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
@@ -14,17 +14,17 @@
 //polygon.bindPopup("I am a polygon.");
 
 //var popup = L.popup()
-  //  .setLatLng([-41.327222, 174.805278])
-  //  .setContent("Hier befindet sich der Flughafen von Wellington.")
-  //  .openOn(mymap);
+//  .setLatLng([-41.327222, 174.805278])
+//  .setContent("Hier befindet sich der Flughafen von Wellington.")
+//  .openOn(mymap);
 
 //var popup = L.popup();
 
 //function onMapClick(e) { 
-  //  popup
-      //  .setLatLng(e.latlng)
-      //  .setContent("You clicked the map at " + e.latlng.toString())
-      //  .openOn(mymap);
+//  popup
+//  .setLatLng(e.latlng)
+//  .setContent("You clicked the map at " + e.latlng.toString())
+//  .openOn(mymap);
 //}
 
 //mymap.on('click', onMapClick);
