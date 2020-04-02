838c74cd96cc8c6faf09bbd8070985c8d8cd6b4b
itzhalpepe <leon.buehrle@t-online.de>
2020-04-02 16:43:43 +0200

    verschiedene Leaflat hinzufügen

--- world/main.js
+++ world/main.js
@@ -1,7 +1,19 @@
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+
 let map = L.map("map", {
     center: [0,0],
     zoom: 2,
-    layers_ [
-        L.tileLayer.provider("OpenTopoMap")
+    layers: [
+        startLayer
     ]
 });
+
+L.control.layers({
+    "OpenTopoMap" : startLayer,
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "CyclOSM" : L.tileLayer.provider("CyclOSM"),
+    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
+    "Stamen.TerrainBackground" : L.tileLayer.provider("Stamen.TerrainBackground"),
+
+
+}).addTo(map)
