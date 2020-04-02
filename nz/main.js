43c0d9cafab9fb9612a659ff1fffd0dff909bf87
itzhalpepe <leon.buehrle@t-online.de>
2020-04-02 16:00:14 +0200

    Anpassen des Codes

--- nz/main.js
+++ nz/main.js
@@ -30,15 +30,38 @@
 //mymap.on('click', onMapClick);
 
 
-let map = document.querySelector("#map")
-var mymap = L.map('map').setView([-41.327222, 174.805278], 13);
+// let map = document.querySelector("#map")
+// var mymap = L.map('map').setView([-41.327222, 174.805278], 13);
 
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(mymap);
+// L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 17,
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// }).addTo(mymap);
 
 
-var popup = L.marker([-41.327222, 174.805278]).addTo(mymap);
+// var popup = L.marker([-41.327222, 174.805278]).addTo(mymap);
 
-popup.bindPopup("<b>Flughafen von Wellington</b><br>Hauptstadt Neuseelands").openPopup();
+// popup.bindPopup("<b>Flughafen von Wellington</b><br>Hauptstadt Neuseelands").openPopup();
+
+let mapdiv = document.querySelector("#map");
+
+let map = L.map("map", {
+    center: [
+        mapdiv.dataset.lat,
+        mapdiv.dataset.lng
+    ],
+    zoom: 17,
+    layers: [
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            attribution: `Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`
+        })
+    ]
+});
+
+let mrk = L.marker([
+    mapdiv.dataset.lat,
+    mapdiv.dataset.lng
+]).addTo(map);
+
+mrk.bindPopup(mapdiv.dataset.title).openPopup();
