aa1bb17dcdc63f88edccf12e410ce75b8403d9e6
itzhalpepe <leon.buehrle@t-online.de>
2020-04-09 17:16:53 +0200

    slider importieren und Kreise bei 0 verschwinden

--- world/main.js
+++ world/main.js
@@ -28,31 +28,71 @@ L.control.layers({
 let drawCircles = function () {
     let data = CONFIRMED;
     let header = CONFIRMED[0];
-    let index = header.length - 1;
-    let topic = "bestätigte Fälle";
+    let index = document.querySelector("#slider").value;
+    let options = document.querySelector("#pulldown").options;
+    let value = options[options.selectedIndex].value;
+    let label = options[options.selectedIndex].text;
+    let color;
+    //console.log(value,label,options);
 
-    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${topic}`;
+    if (value === "confirmed") {
+        data = CONFIRMED;
+        color = "#0074D9";
+    } else if (value === "deaths") {
+        data = DEATHS;
+        color = "#B10DC9";
+    } else {
+        data = RECOVERED;
+        color = "#2ECC40";
+    }
 
-    // for-Schleife über alle Arrays der CONFIRMED Einträge:
+    console.log(CONFIRMED == RECOVERED);
 
+    // Datum & Thema anzeigen anzeigen
+    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
+
+    circleGroup.clearLayers();
+
+    //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
-        // console.log(row[2],row[3]);
+        //console.log(row[2],row[3]);
         let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
         let val = row[index];
-        // let mrk = L.marker([row[2],row[3]]).addTo(map);
-        // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
+
+        if (val === "0"){
+            continue;
+        }
+
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
         let s = 0.5;
         let r = Math.sqrt(val * s / Math.PI);
         let circle = L.circleMarker([lat, lng], {
-            radius: r
+            radius: r,
+            color: color
         }).addTo(circleGroup);
         circle.bindPopup(`${reg}: ${val}`);
     }
 };
 
+document.querySelector("#pulldown").onchange = function() {
+    drawCircles();
+};
+
+let slider = document.querySelector("#slider");
+slider.min = 4;
+slider.max = CONFIRMED[0].length - 1;
+slider.step = 1;
+slider.value = slider.max;
+
+slider.onchange = function () {
+    drawCircles();
+};
+
 drawCircles();
-// drawCircles(RECOVERED);
-// drawCircles(DEATHS);
