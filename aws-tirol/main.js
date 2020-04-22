f6f5d283f38b2a90d60be45d7dd317a0e5af543e
itzhalpepe <leon.buehrle@t-online.de>
2020-04-22 17:52:48 +0200

    Einfügen der restlichen Paramter

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -43,6 +43,8 @@ let aws = L.geoJson.ajax(awsUrl, {
         <li>Datum: ${point.properties.date}</li>
         <li>Lufttemperatur: ${point.properties.LT} °C</li>
         <li>Windgeschwindigkeit: ${point.properties.WG} m/s</li>
+        <li>Relative Luftfeuchte: ${point.properties.RH} %</li>
+        <li>Schneehöhe: ${point.properties.HS} cm</li>
         </ul>
         `);
         return marker;
