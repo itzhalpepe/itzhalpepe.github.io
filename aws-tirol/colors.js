090313bd1c845ef2b51edbcdc8f015be62411b8d
itzhalpepe <leon.buehrle@t-online.de>
2020-04-23 17:17:45 +0200

    colors hinzufügen

---
+++ aws-tirol/colors.js
@@ -0,0 +1,47 @@
+Temperatur
+<rect class="legend-rect" width="41" height="18" x="0" y="0" fill="rgb(250,55,150)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="18" fill="rgb(255,5,5)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="36" fill="rgb(255,85,54)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="54" fill="rgb(255,154,53)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="72" fill="rgb(255,190,130)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="90" fill="rgb(255,255,103)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="108" fill="rgb(176,255,188)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="126" fill="rgb(140,255,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="144" fill="rgb(3,205,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="162" fill="rgb(4,129,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="180" fill="rgb(3,91,190)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="198" fill="rgb(120,75,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="216" fill="rgb(159,128,255)"></rect>
+<line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line>
+<line class="legend-border" x1="0" x2="41" y1="234" y2="234"></line><text class="legend-text" x="20.5" y="18"
+    dy="0.35em" font-size="12" text-anchor="middle">30</text><text class="legend-text" x="20.5" y="36" dy="0.35em"
+    font-size="12" text-anchor="middle">25</text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12"
+    text-anchor="middle">20</text><text class="legend-text" x="20.5" y="72" dy="0.35em" font-size="12"
+    text-anchor="middle">15</text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12"
+    text-anchor="middle">10</text><text class="legend-text" x="20.5" y="108" dy="0.35em" font-size="12"
+    text-anchor="middle">5</text><text class="legend-text" x="20.5" y="126" dy="0.35em" font-size="12"
+    text-anchor="middle">0</text><text class="legend-text" x="20.5" y="144" dy="0.35em" font-size="12"
+    text-anchor="middle">-5</text><text class="legend-text" x="20.5" y="162" dy="0.35em" font-size="12"
+    text-anchor="middle">-10</text><text class="legend-text" x="20.5" y="180" dy="0.35em" font-size="12"
+    text-anchor="middle">-15</text><text class="legend-text" x="20.5" y="198" dy="0.35em" font-size="12"
+    text-anchor="middle">-20</text><text class="legend-text" x="20.5" y="216" dy="0.35em" font-size="12"
+    text-anchor="middle">-25</text>
+
+
+
+Wind
+<rect class="legend-rect" width="41" height="18" x="0" y="0" fill="rgb(255,50,50)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="18" fill="rgb(150,100,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="36" fill="rgb(100,150,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="54" fill="rgb(50,200,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="72" fill="rgb(150,255,150)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="90" fill="rgb(200,255,100)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="108" fill="rgb(255,255,100)"></rect>
+<line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line>
+<line class="legend-border" x1="0" x2="41" y1="126" y2="126"></line><text class="legend-text" x="20.5" y="18"
+    dy="0.35em" font-size="12" text-anchor="middle">80</text><text class="legend-text" x="20.5" y="36" dy="0.35em"
+    font-size="12" text-anchor="middle">60</text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12"
+    text-anchor="middle">40</text><text class="legend-text" x="20.5" y="72" dy="0.35em" font-size="12"
+    text-anchor="middle">20</text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12"
+    text-anchor="middle">10</text><text class="legend-text" x="20.5" y="108" dy="0.35em" font-size="12"
+    text-anchor="middle">5</text>
