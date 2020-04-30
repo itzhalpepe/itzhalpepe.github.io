40dad95c85f402a141650a5cf46b508d72e79396
itzhalpepe <leon.buehrle@t-online.de>
2020-04-30 17:54:58 +0200

    Etappenpulldown erzeugt

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -9,7 +9,8 @@ let map = L.map("map", {
 });
 
 let overlay = {
-    adlerblicke: L.featureGroup()
+    adlerblicke: L.featureGroup(),
+    etappen: L.featureGroup()
 };
 
 L.control.layers({
@@ -25,7 +26,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Adlerblicke": overlay.adlerblicke
+    "Adlerblicke": overlay.adlerblicke,
+    "Etappen": overlay.etappen
 }).addTo(map);
 
 //console.log(ETAPPEN);
@@ -46,22 +48,40 @@ for (const blick of ADLERBLICKE) {
 }
 overlay.adlerblicke.addTo(map);
 
-let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
-    async: true,
-    marker_options: {
-        startIconUrl: "icons/number_1.png",
-        endIconUrl: "icons/finish.png",
-        shadowUrl: null,
-        iconSize: [32, 37],
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    },
-    polyline_options: {
-        color: "black",
-        dashArray: [2, 5]
-    }
-});
+let drawEtappe = function(nr) {
+    let track = ETAPPEN[nr].track.replace("A", "");
+    let gpx = new L.GPX(`gpx/AdlerwegEtappe${track}.gpx`, {
+        async: true,
+        marker_options: {
+            startIconUrl: "icons/number_1.png",
+            endIconUrl: "icons/finish.png",
+            shadowUrl: null,
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],
+            popupAnchor: [0, -37]
+        },
+        polyline_options: {
+            color: "black",
+            dashArray: [2, 5]
+        }
+    });
+    
+    gpx.on("loaded", function(evt) {
+        map.fitBounds(evt.target.getBounds());
+    }).addTo(overlay.etappen);
+    overlay.etappen.addTo(map);
+    console.log(nr);
+};
+drawEtappe(22);
 
-gpx.on("loaded", function(evt) {
-    map.fitBounds(evt.target.getBounds());
-}).addTo(map);
+let pulldown = document.querySelector("#pulldown");
+console.log(pulldown);
+
+for (let i=1; i < ETAPPEN.length; i++) {
+    const etappe = ETAPPEN[i];
+    console.log(etappe);
+    pulldown.innerHTML += `<option value = "${i}">${etappe.titel}</option>`;
+}
+pulldown
+
+
