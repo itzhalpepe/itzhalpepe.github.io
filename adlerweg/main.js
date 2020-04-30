91869359cdecb4a3c11e1aed6de9cd531d6367eb
itzhalpepe <leon.buehrle@t-online.de>
2020-04-30 17:30:32 +0200

    Nummern hinzufügen und Polyline bei main.js

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -28,8 +28,11 @@ L.control.layers({
     "Adlerblicke": overlay.adlerblicke
 }).addTo(map);
 
+//console.log(ETAPPEN);
+//console.log(ADLERBLICKE);
+
 for (const blick of ADLERBLICKE) {
-    console.log(blick);
+    //console.log(blick);
     let mrk = L.marker([blick.lat,blick.lng], {
         icon: L.icon({
             iconSize: [32, 37],
@@ -38,15 +41,27 @@ for (const blick of ADLERBLICKE) {
             iconUrl: "icons/panoramicview.png"
         })
     }).addTo(overlay.adlerblicke);
-    L.marker([blick.lat,blick.lng]).addTo(map);
+    //L.marker([blick.lat,blick.lng]).addTo(map);
     mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
 }
 overlay.adlerblicke.addTo(map);
 
-let gpx = new KeyboardEvent.GPX("gpx/AdlerwegEtape01.gpx", {
-    async: true
+let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
+    async: true,
+    marker_options: {
+        startIconUrl: "icons/number_1.png",
+        endIconUrl: "icons/finish.png",
+        shadowUrl: null,
+        iconSize: [32, 37],
+        iconAnchor: [16, 37],
+        popupAnchor: [0, -37]
+    },
+    polyline_options: {
+        color: "black",
+        dashArray: [2, 5]
+    }
 });
 
-gpx. on("loaded", function(evt) {
+gpx.on("loaded", function(evt) {
     map.fitBounds(evt.target.getBounds());
 }).addTo(map);
