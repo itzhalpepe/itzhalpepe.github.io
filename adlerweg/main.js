31de715e841c4834704ab40eefa03a12946cef06
itzhalpepe <leon.buehrle@t-online.de>
2020-04-30 16:29:58 +0200

    overlay hinzufügen

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,6 +8,10 @@ let map = L.map("map", {
     ]
 });
 
+let overlay = {
+    adlerblicke: L.featureGroup()
+};
+
 L.control.layers({
     "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,6 +24,12 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Adlerblicke": overlay.adlerblicke
 }).addTo(map);
 
-console.log(ETAPPEN)
+for (const blick of ADLERBLICKE) {
+            let mrk = L.marker([blick.lat, blick.lng]).addTo(map);
+            mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
+}
+overlay.adlerblicke.addTo(map);
